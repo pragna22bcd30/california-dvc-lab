e439d1b6e335b11221900f38f623079353eddc4a
pragna2022bcd0030 <pragnachennur@gmail.com>
2026-03-16 23:33:49 +0530

    Add training script

---
+++ src/train.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load dataset
+data = pd.read_csv("data/housing.csv")
+
+# Simple preprocessing
+data = data.select_dtypes(include=["float64", "int64"])
+
+X = data.drop("median_house_value", axis=1)
+y = data["median_house_value"]
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predictions
+pred = model.predict(X_test)
+
+# Metrics
+rmse = mean_squared_error(y_test, pred, squared=False)
+r2 = r2_score(y_test, pred)
+
+print("Dataset size:", len(data))
+print("RMSE:", rmse)
+print("R2:", r2)
